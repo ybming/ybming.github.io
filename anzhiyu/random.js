2277283ae38be9e61cd34dd9c04e8841501be259
ybming <88264029@qq.com>
2025-09-27 12:04:52 +0800

    Site updated: 2025-09-27 12:04:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/26/hello-world/"];function toRandomPost(){
+var posts=["2025/09/26/hello-world/","2024/01/20/diyi/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
