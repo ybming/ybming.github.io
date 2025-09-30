249d98276029b6e664a74e2632b4dcc67c86a223
ybming <88264029@qq.com>
2025-09-30 18:48:24 +0800

    Site updated: 2025-09-30 18:48:23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759225868188";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1759229262146";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
